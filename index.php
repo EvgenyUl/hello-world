9a8e6d4b9945419daa3c59ed02182ba524e9ee4a
Eugene Ul <evgenyulchiev@gmail.com>
2022-05-05 09:56:15 +0300

    Step 10: Functions
    Please enter the commit message for your changes.
    
    Lines starting with '#' will be ignore, and an empty
    
    message aborts the commit.
    
    Date: Thu May 5 09:56:15 2022 +0300
    
    On Branch main
    
    Your branch is up to date with 'origin/main'.

--- index.php
+++ index.php
@@ -2,11 +2,15 @@
 error_reporting(-1);
 ini_set('display_errors', '1');
 
+require_once 'functions.php';
+
 $person = [
   'name' => 'John',
   'age' => 32,
   'hair' => 'brown',
   'prof' => 'web developer',
 ];
+
+dd($person);
 
 include_once 'index.view.php';
