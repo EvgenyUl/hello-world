d8322896a3aad87e67b8ccd36c812bf0abbef23a
Evgeny <evgenyulchiev@gmail.com>
2022-05-08 09:38:29 +0300

    Redirect route

--- web.php
+++ web.php
@@ -6,3 +6,7 @@ Route::get('/', [ControllerController::class, 'method']);
 Route::any('/', function(){
 	return 'Hello, World!';
 });
+
+Route::redirect('/here', '/there');
+Route::redirect('/here', '/there', 301);
+Route::permanentRedirect('/here', '/there');
